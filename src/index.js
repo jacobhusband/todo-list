403741a0a7b9a247d6485e79668d75c84343b09f
Jacob Husband <jakehusband2@gmail.com>
2023-01-14 20:22:32 -0800

    Implement the needed methods for the time controller

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const evt = new EventEmitter();
-
-evt.subscribe("Piss", (value) => console.log(value));
-
-evt.emit("Piss", 19);
+import "./css/reset.css";
+import "./css/layout.css";
+import "./css/styles.css";
+import TimeController from "./js/modules/timeController";
--- src/js/modules/timeController.js
+++ src/js/modules/timeController.js
@@ -1,1 +1,25 @@
-export default class TimeController {}
+import { format, differenceInMilliseconds } from "date-fns";
+
+export default class TimeController {
+  constructor() {}
+
+  getCurrentLocalTime() {
+    this.currentLocalTime = format(new Date(), "h:mm a");
+    return this.currentLocalTime;
+  }
+
+  getCurrentLocalDate() {
+    this.currentLocalDate = format(new Date(), "yyyy-MM-dd");
+    return this.currentLocalDate;
+  }
+
+  getCurrentDateAndTime() {
+    this.currentDate = new Date();
+    return this.currentDate;
+  }
+
+  getDifferenceBetweenTimes(time1, time2) {
+    this.difference = differenceInMilliseconds(time2, time1);
+    return this.difference;
+  }
+}
