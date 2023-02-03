c8aff1e641785ae72ed5b6896d51a6ac323d8f53
Jacob Husband <jakehusband2@gmail.com>
2023-02-03 10:30:17 -0800

    Add projects modal

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import TaskManager from "./js/taskManager";
 import EventManager from "./js/eventManager";
 import UIManager from "./js/uiManager";
 
-const tm = new TaskManager();
+const tm = new TaskManager("Default", 0);
 const em = new EventManager();
 const uim = new UIManager();
 
